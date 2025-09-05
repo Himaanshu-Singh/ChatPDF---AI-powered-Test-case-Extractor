906639614f8497fef9e9985e808e6c32f92548ff
Himanshu Singh <89242788+Himaanshu-Singh@users.noreply.github.com>
2025-09-05 12:55:13 +0530

    Update test_api.py

--- chatbot/test_api.py
+++ chatbot/test_api.py
@@ -1,12 +1,11 @@
-#3b5ec8d2-bbe3-45fe-a308-51fa2a6f253d
-#f07c26e6-195a-482d-86d2-3bfba5a3e8df       -  shockwave id ai21 labs
+
 
 from ai21 import AI21Client
 from ai21.models.chat import ChatMessage
 
 client = AI21Client(
-    api_key='3b5ec8d2-bbe3-45fe-a308-51fa2a6f253d',  # No #
-)
+)    api_key='apikey',  # No #
+
 
 system = "You're a support engineer in a SaaS company"
 messages = [
@@ -20,3 +19,4 @@ chat_completions = client.chat.completions.create(
 )
 
 print(chat_completions.choices[0].message.content)
+
